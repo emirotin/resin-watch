67c11d8ffdfec60aa5884d62860e6b402803bc04
Praneeth Bodduluri <praneeth@bufferlabs.in>
2014-07-11 18:52:29 +0530

    Digitiser prototype

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var tty = require('tty.js');
+var spawn = require('child_process').spawn;
+
 // Simple tty.js in app mode
 var app = tty.createServer({
   shell: 'bash',
@@ -7,5 +9,11 @@ var app = tty.createServer({
   },
   port: process.env.PORT
 });
+spawn('/app/spawn_screen',[''],
+			{
+			    detached: true,
+			    stdio: [ 'ignore', 'ignore', 'ignore' ]
+			}
+			);
 
 app.listen();
