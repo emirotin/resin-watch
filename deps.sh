4126deef2c28d70bbcfa619e1e1a6681c314488a
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-03-09 13:50:56 +0000

    install aptitude

--- deps.sh
+++ deps.sh
@@ -1,3 +1,3 @@
 apt-get update
-apt-get upgrade -y 
-apt-get install -y matchbox chromium x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3 xinit libraspberrypi-bin fbset htop psmisc
+apt-get upgrade -y && apt-get install -y aptitude
+aptitude install -y matchbox chromium x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3 xinit libraspberrypi-bin fbset htop psmisc
